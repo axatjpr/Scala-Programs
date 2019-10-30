d5ff589e75b00a8fd1dfeff068be0d7faeb33d41
axatjpr <axatjpr@hotmail.com>
2019-10-30 11:17:23 +0530

    Add files via upload

---
+++ hello.scala
@@ -0,0 +1,31 @@
+////object Hello extends App {
+////  println("Hello, World!")
+////}
+//
+//def example = 2
+//val example = 2
+//lazy val example =2
+//
+//def square(x:Double)
+//def square(x: => Double)
+//
+//object Hello extends App{
+//  println("Hello World")
+//
+//}
+//
+//object Hello{
+//  def main(args:Array[String]): Unit ={
+//    println("Helloworld2")
+//  }
+//}
+
+object abc{
+  def and(x:Boolean,y : Int)= if (x) y else false
+  //1
+  // and: (x:Boolean, y:Boolean)Boolean
+  and(true,false)d
+}
+
+
+
