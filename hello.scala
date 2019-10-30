35a3319241dd8e1b380a9e905eda18d4347f3914
axatjpr <axatjpr@hotmail.com>
2019-10-30 11:18:11 +0530

    Update hello.scala

--- hello.scala
+++ hello.scala
@@ -1,30 +1,23 @@
-////object Hello extends App {
-////  println("Hello, World!")
-////}
-//
-//def example = 2
-//val example = 2
-//lazy val example =2
-//
-//def square(x:Double)
-//def square(x: => Double)
-//
-//object Hello extends App{
-//  println("Hello World")
-//
-//}
-//
-//object Hello{
-//  def main(args:Array[String]): Unit ={
-//    println("Helloworld2")
-//  }
-//}
+object Hello extends App {
+ println("Hello, World!")
+}
 
-object abc{
-  def and(x:Boolean,y : Int)= if (x) y else false
-  //1
-  // and: (x:Boolean, y:Boolean)Boolean
-  and(true,false)d
+def example = 2
+val example = 2
+lazy val example =2
+
+def square(x:Double)
+def square(x: => Double)
+
+object Hello extends App{
+ println("Hello World")
+
+}
+
+object Hello{
+ def main(args:Array[String]): Unit ={
+   println("Helloworld2")
+ }
 }
 
 
